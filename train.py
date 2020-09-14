03cb8e896bc082a9fb589292aff6bf3f8cc0be2a
Carlos Sosa <c.sosa@pucp.edu.pe>
2020-09-14 09:49:08 -0500

    epochs

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def main():
         help='path in which numpy arrays are stored as numPy arrays')
     arg('--batch-size', type=int, default=4, help='HR:4,VHR:8')
     arg('--limit', type=int, default=10000, help='number of images in epoch')
-    arg('--n-epochs', type=int, default=400)
+    arg('--n-epochs', type=int, default=200)
     arg('--lr', type=float, default=1e-3)
 
     args = parser.parse_args()
