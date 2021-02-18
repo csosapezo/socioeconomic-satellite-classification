256e3db507f3c23b8323754ac56cf799907cc0ff
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-02-17 20:04:11 -0500

    fix

--- spatialite_test.py
+++ spatialite_test.py
@@ -15,9 +15,9 @@ end_y = input("Y final:")
 
 search_polygon = 'POLYGON(({0} {1}, {0} {2}, {3} {1}, {3} {2}, {0} {1}))'.format(start_x, start_y, end_y, end_x)
 
-select = 'SELECT AsText(GeomFromWKB(GEOMETRY))'
-from_where = 'FROM planos WHERE ST_Intersects(GeomFromText("{}"), GeometryN(GeomFromWKB(GEOMETRY),1)) = 1'\
-    .format(search_polygon)
+select = 'select ST_Intersects(GeomFromText("{}"), GeometryN(GeomFromWKB(GEOMETRY),1)) as booll, AsText(GeometryN(' \
+         'GeomFromWKB(GEOMETRY),1))'.format(search_polygon)
+from_where = 'from planos where booll = 1;'
 query = select + ' ' + from_where
 
 print("Consulta:")
