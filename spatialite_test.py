cfe66d5441f2b4e6f38b858d09f6968529a75710
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-02-17 19:07:19 -0500

    fix

--- spatialite_test.py
+++ spatialite_test.py
@@ -9,17 +9,14 @@ connR.execute('SELECT InitSpatialMetaData(1);')
 
 print("Introduza el área a buscar")
 start_x = input("X inicial:")
-offset_x = input("offset:")
+end_x = input("X final:")
 start_y = input("Y inicial:")
-offset_y = input("offset:")
-
-end_x = start_x + offset_x
-end_y = start_y + offset_y
+end_y = input("Y final:")
 
 search_polygon = 'POLYGON(({0} {1}, {0} {2}, {3} {1}, {3} {2}, {0} {1}))'.format(start_x, start_y, end_y, end_x)
 
 select = 'SELECT AsText(GeomFromWKB(GEOMETRY))'
-from_where = 'FROM planos WHERE ST_Intersects(GeomFromText("{}"), GeometryN(GeomFromWKB(GEOMETRY),1)) = 1 '\
+from_where = 'FROM planos WHERE ST_Intersects(GeomFromText("{}"), GeometryN(GeomFromWKB(GEOMETRY),1)) = 1'\
     .format(search_polygon)
 query = select + ' ' + from_where
 
