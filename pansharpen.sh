1d85a98521fa160509a5d72b267af2cfb89b2257
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-03-30 08:27:26 -0500

    add pansharpen script

---
+++ pansharpen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Variables
+origin_dir=$1
+output_dir=$2
+search_term=$3
+
+# Search files
+list=$(find $origin_dir -maxdepth 2 -type d -name "*$search_term*")
+
+mkdir $output_dir
+
+for folder in $list
+do
+	foldername=$(basename $folder)
+	ms_tif=$(find $folder -name "*ORT_MS*.TIF")
+	p_tif=$(find $folder -name "*ORT_P*.TIF")
+
+	echo ${p_tif[0]}
+	echo ${ms_tif[0]}
+
+	gdal_pansharpen.py ${p_tif[0]} ${ms_tif[0]} "$output_dir/$foldername.TIF"
+done
