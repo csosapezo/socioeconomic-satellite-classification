da68e82e7544cb73c54056edcd92031fa8e61542
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-03-30 13:09:48 -0500

    minor fixes

--- pansharpen.sh
+++ pansharpen.sh
@@ -6,18 +6,21 @@ output_dir=$2
 search_term=$3
 
 # Search files
-list=$(find $origin_dir -maxdepth 2 -type d -name "*$search_term*")
+list=$(find "$origin_dir" -maxdepth 2 -type d -name "*$search_term*")
 
-mkdir $output_dir
+mkdir "$output_dir"
 
 for folder in $list
 do
-	foldername=$(basename $folder)
-	ms_tif=$(find $folder -name "*ORT_MS*.TIF")
+	foldername=$(basename "$folder")
+	ms_tif=$(find "$folder" -name "*ORT_MS*.TIF")
+	# shellcheck disable=SC2086
 	p_tif=$(find $folder -name "*ORT_P*.TIF")
 
+	# shellcheck disable=SC2086
 	echo ${p_tif[0]}
-	echo ${ms_tif[0]}
+	echo "${ms_tif[0]}"
 
-	gdal_pansharpen.py ${p_tif[0]} ${ms_tif[0]} "$output_dir/$foldername.TIF"
+	# shellcheck disable=SC2086
+	gdal_pansharpen.py "${p_tif[0]}" ${ms_tif[0]} "$output_dir/$foldername.TIF"
 done
