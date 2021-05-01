560f63bafbeda5be60d4d73cb4f1856ae06e5eb6
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-01 12:58:01 -0500

    change unet for ternausnet

--- models/unet.py
+++ models/unet.py
@@ -1,99 +1,121 @@
 import torch
-import torch.nn as nn
-from torch.nn import functional
-
-"""
-UNet PyTorch
-Implementation adapted from https://github.com/usuyama/pytorch-unet
-"""
+from torch import nn
+from torch.nn import functional as F
+from torchvision import models
 
 
-def double_conv(in_channels, out_channels):
-    """
-    Double convolutional layer + ReLU activation
-
-    :param in_channels: input channels
-    :type in_channels: int
-
-    :param out_channels: output channels
-    :type out_channels: int
-
-    :return: double convolutional layer (nn.Sequential)
-    """
-
-    return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, 3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(out_channels, out_channels, 3, padding=1),
-        nn.ReLU(inplace=True)
-    )
+# ----------------------------------------------------------------------------
+def conv3x3(in_, out):
+    return nn.Conv2d(in_, out, 3, padding=1)
 
 
-class UNet(nn.Module):
-    """
-        Base UNet implementation
-    """
-
-    def __init__(self, input_channels=4, n_class=1):
-        """
-        Constructor method for UNet
-
-        :param input_channels: input channels
-        :param n_class: number of segmentation classes
-        """
-        self.n_class = n_class
+class ConvRelu(nn.Module):
+    def __init__(self, in_: int, out: int):
         super().__init__()
-
-        self.dconv_down1 = double_conv(input_channels, 64)
-        self.dconv_down2 = double_conv(64, 128)
-        self.dconv_down3 = double_conv(128, 256)
-        self.dconv_down4 = double_conv(256, 512)
-
-        self.maxpool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-
-        self.dconv_up3 = double_conv(256 + 512, 256)
-        self.dconv_up2 = double_conv(128 + 256, 128)
-        self.dconv_up1 = double_conv(128 + 64, 64)
-
-        self.conv_last = nn.Conv2d(64, n_class, 1)
+        self.conv = conv3x3(in_, out)
+        self.activation = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        """
-        UNet Forward method
-        :param x: input layer
+        x = self.conv(x)
+        x = self.activation(x)
+        return x
 
-        :return: segmented layer
-        """
 
-        conv1 = self.dconv_down1(x)
-        x = self.maxpool(conv1)
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, is_deconv=True):
+        super(DecoderBlock, self).__init__()
+        self.in_channels = in_channels
 
-        conv2 = self.dconv_down2(x)
-        x = self.maxpool(conv2)
-
-        conv3 = self.dconv_down3(x)
-        x = self.maxpool(conv3)
-
-        x = self.dconv_down4(x)
-
-        x = self.upsample(x)
-        x = torch.cat([x, conv3], dim=1)
-
-        x = self.dconv_up3(x)
-        x = self.upsample(x)
-        x = torch.cat([x, conv2], dim=1)
-
-        x = self.dconv_up2(x)
-        x = self.upsample(x)
-        x = torch.cat([x, conv1], dim=1)
-
-        x = self.dconv_up1(x)
-
-        if self.n_class > 1:
-            x = self.conv_last(x)
-            out = functional.log_softmax(x, dim=1)
+        if is_deconv:
+            self.block = nn.Sequential(
+                ConvRelu(in_channels, middle_channels),
+                nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=4, stride=2,
+                                   padding=1),
+                nn.ReLU(inplace=True)
+            )
         else:
-            out = self.conv_last(x)
+            self.block = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode='nearest'),
 
-        return out
+                # nn.Upsample(scale_factor=2, mode='bilinear'),
+                ConvRelu(in_channels, middle_channels),
+                ConvRelu(middle_channels, out_channels),
+            )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class UNet11(nn.Module):
+    def __init__(self, num_classes=1, num_filters=32, pretrained=False, input_channels=4, is_deconv=False):
+        """
+        :param num_classes:
+        :param num_filters:
+        :param pretrained:
+            False - no pre-trained network used
+            vgg - encoder pre-trained with VGG11
+        """
+        super().__init__()
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.num_classes = num_classes
+
+        self.encoder = models.vgg11(pretrained=pretrained).features
+
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Sequential(nn.Conv2d(input_channels, 64, 3, padding=1, bias=False),
+                                   self.relu)
+
+        self.conv2 = nn.Sequential(self.encoder[3],
+                                   self.relu)
+
+        self.conv3 = nn.Sequential(
+            self.encoder[6],
+            self.relu,
+            self.encoder[8],
+            self.relu,
+        )
+        self.conv4 = nn.Sequential(
+            self.encoder[11],
+            self.relu,
+            self.encoder[13],
+            self.relu,
+        )
+
+        self.conv5 = nn.Sequential(
+            self.encoder[16],
+            self.relu,
+            self.encoder[18],
+            self.relu,
+        )
+
+        self.center = DecoderBlock(256 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv=is_deconv)
+        self.dec5 = DecoderBlock(512 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv=is_deconv)
+        self.dec4 = DecoderBlock(512 + num_filters * 8, num_filters * 8 * 2, num_filters * 4, is_deconv=is_deconv)
+        self.dec3 = DecoderBlock(256 + num_filters * 4, num_filters * 4 * 2, num_filters * 2, is_deconv=is_deconv)
+        self.dec2 = DecoderBlock(128 + num_filters * 2, num_filters * 2 * 2, num_filters, is_deconv=is_deconv)
+        self.dec1 = ConvRelu(64 + num_filters, num_filters)
+
+        self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        conv1 = self.conv1(x)
+        conv2 = self.conv2(self.pool(conv1))
+        conv3 = self.conv3(self.pool(conv2))
+        conv4 = self.conv4(self.pool(conv3))
+        conv5 = self.conv5(self.pool(conv4))
+        center = self.center(self.pool(conv5))
+
+        dec5 = self.dec5(torch.cat([center, conv5], 1))
+        dec4 = self.dec4(torch.cat([dec5, conv4], 1))
+        dec3 = self.dec3(torch.cat([dec4, conv3], 1))  #
+        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
+        dec1 = self.dec1(torch.cat([dec2, conv1], 1))
+
+        if self.num_classes > 1:
+            x_out = F.log_softmax(self.final(dec1), dim=1)
+        else:
+            x_out = self.final(dec1)
+
+        # return x_out, conv1
+        return x_out
