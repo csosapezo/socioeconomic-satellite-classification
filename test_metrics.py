3fc653b66269e5f2a8b5c8fb6724a1eb9f396d37
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-21 15:21:19 -0500

    test

--- test_metrics.py
+++ test_metrics.py
@@ -90,15 +90,15 @@ def test_metrics():
         y = pickle.load(open(mask_path, "rb"))
         print("Get mask for sample {}".format(idx))
 
-        fig.add_subplot(args.num_picture, 3, idx + 1)
+        fig.add_subplot(args.num_picture, 3, idx * 3 + 1)
         plt.imshow(reverse_transform(image.cpu().numpy()[0]))
         print("Add plot for sample input {}".format(idx))
 
-        fig.add_subplot(args.num_picture, 3, idx + 2)
+        fig.add_subplot(args.num_picture, 3, idx * 3 + 2)
         plt.imshow(masks_to_colorimg(y))
         print("Add plot for sample mask {}".format(idx))
 
-        fig.add_subplot(args.num_picture, 3, idx + 3)
+        fig.add_subplot(args.num_picture, 3, idx * 3 + 3)
         plt.imshow(masks_to_colorimg(pred.cpu().numpy()[0]))
         print("Add plot for sample pred {}".format(idx))
 
