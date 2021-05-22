98415ee1b4d1678276e6b4d00744b4d1fcc3bc32
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-22 13:23:13 -0500

    filter

--- sample_dataset.py
+++ sample_dataset.py
@@ -2,18 +2,18 @@ import csv
 import os
 import shutil
 
-split_dir = "./data/dataset/split"
-new_dir = "./data/dataset/split2"
+split_dir = "./data/dataset/old_split"
+new_dir = "./data/dataset/split"
 aux_dir = '.data/dataset/old_split2'
 
-with open('new_subdata.csv', newline='') as f:
+with open('new_subdata_2.csv', newline='') as f:
     reader = list(csv.reader(f))
     sample = [x[0] for x in reader]
 
-os.mkdir(new_dir)
+# os.mkdir(new_dir)
 
 for basename in sample:
     os.rename(os.path.join(split_dir, basename), os.path.join(new_dir, basename))
 
-shutil.move(split_dir, aux_dir)
-shutil.move(new_dir, split_dir)
+# shutil.move(split_dir, aux_dir)
+# shutil.move(new_dir, split_dir)
