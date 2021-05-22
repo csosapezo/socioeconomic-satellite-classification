0a26bc86d178123759f2c3b426e2dc05eb0667ee
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-22 13:17:53 -0500

    new sample

--- sample_dataset.py
+++ sample_dataset.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import shutil
 
 split_dir = "./data/dataset/split"
 new_dir = "./data/dataset/split2"
@@ -14,5 +15,5 @@ os.mkdir(new_dir)
 for basename in sample:
     os.rename(os.path.join(split_dir, basename), os.path.join(new_dir, basename))
 
-os.rename(split_dir, aux_dir)
-os.rename(new_dir, split_dir)
+shutil.move(split_dir, aux_dir)
+shutil.move(new_dir, split_dir)
