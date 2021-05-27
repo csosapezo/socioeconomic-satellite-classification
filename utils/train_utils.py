80de8e44320d4477ccc998fbc513efc0f14bdbd3
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-27 02:44:32 -0500

    fix cuda assert error

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -76,9 +76,6 @@ def train_model(name_file, model, dataset, optimizer, scheduler, dataloaders, na
 
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs)
-                    print("pred", outputs.shape)
-                    print("target", labels.shape)
-                    input()
                     loss = calc_loss(outputs, labels, metrics, dataset)
 
                     if phase == 'train':
