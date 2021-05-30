3ff4e68f277a2037646684a7850d87403bcabfc6
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-30 03:20:06 -0500

    clean

--- deletee.py
+++ deletee.py
@@ -15,24 +15,24 @@ for image in images:
 
     unified = mask[0] + mask[1]
 
-    if unified.max() == 1 and mask.max() == 1:
+    if unified.max() == 1 and mask.max() == 1 and mask.min() == 0:
         continue
 
-    print(image, "max:", mask.max(), "unified_max:", unified.max())
+    print(image, "max:", mask.max(), "unified_max:", unified.max(), "min:", mask.min())
 
     mask = mask.astype(bool).astype(int)
 
-    fig = plt.figure(figsize=(30, 10))
+    # fig = plt.figure(figsize=(30, 10))
 
-    sp1 = fig.add_subplot(1, 3, 1)
-    fig.colorbar(plt.imshow(mask[0], vmin=0, vmax=1))
+    # sp1 = fig.add_subplot(1, 3, 1)
+    # fig.colorbar(plt.imshow(mask[0], vmin=0, vmax=1))
 
-    sp2 = fig.add_subplot(1, 3, 2)
-    fig.colorbar(plt.imshow(mask[1], vmin=0, vmax=1))
+    # sp2 = fig.add_subplot(1, 3, 2)
+    # fig.colorbar(plt.imshow(mask[1], vmin=0, vmax=1))
 
-    sp3 = fig.add_subplot(1, 3, 3)
-    fig.colorbar(plt.imshow(unified, vmin=0, vmax=1))
+    # sp3 = fig.add_subplot(1, 3, 3)
+    # fig.colorbar(plt.imshow(unified, vmin=0, vmax=1))
 
-    plt.savefig(os.path.join(outpath, image[image.rfind("/") + 1:image.rfind(".")] + ".png"))
-    plt.clf()
-    plt.close()
+    # plt.savefig(os.path.join(outpath, image[image.rfind("/") + 1:image.rfind(".")] + ".png"))
+    # plt.clf()
+    # plt.close()
