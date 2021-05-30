a50721c9dfa3def96331c27ec93915bc7ba5eb62
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-30 02:51:00 -0500

    test

--- deletee.py
+++ deletee.py
@@ -25,13 +25,13 @@ for image in images:
     fig = plt.figure(figsize=(30, 10))
 
     sp1 = fig.add_subplot(1, 3, 1)
-    fig.colorbar(plt.imshow(mask[0], vmin=0, vmax=unified.max()))
+    fig.colorbar(plt.imshow(mask[0], vmin=0, vmax=1))
 
     sp2 = fig.add_subplot(1, 3, 2)
-    fig.colorbar(plt.imshow(mask[1], vmin=0, vmax=unified.max()))
+    fig.colorbar(plt.imshow(mask[1], vmin=0, vmax=1))
 
     sp3 = fig.add_subplot(1, 3, 3)
-    fig.colorbar(plt.imshow(unified, vmin=0, vmax=unified.max()))
+    fig.colorbar(plt.imshow(unified, vmin=0, vmax=1))
 
     plt.savefig(os.path.join(outpath, image[image.rfind("/") + 1:image.rfind(".")] + ".png"))
     plt.clf()
