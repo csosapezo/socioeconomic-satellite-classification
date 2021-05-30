80f1d91e90536587e147f73ec81574b6c7f1645d
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-30 02:29:05 -0500

    test

--- deletee.py
+++ deletee.py
@@ -18,22 +18,13 @@ for image in images:
     fig = plt.figure(figsize=(30, 10))
 
     sp1 = fig.add_subplot(1, 3, 1)
-    plt.imshow(mask[0], vmin=0, vmax=unified.max())
-    divider1 = make_axes_locatable(sp1)
-    cax1 = divider1.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(sp1, cax=cax1)
+    fig.colorbar(plt.imshow(mask[0], vmin=0, vmax=unified.max()))
 
     sp2 = fig.add_subplot(1, 3, 2)
-    plt.imshow(mask[1], vmin=0, vmax=unified.max())
-    divider2 = make_axes_locatable(sp2)
-    cax2 = divider2.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(sp2, cax=cax2)
+    fig.colorbar(plt.imshow(mask[1], vmin=0, vmax=unified.max()))
 
     sp3 = fig.add_subplot(1, 3, 3)
-    plt.imshow(mask[1], vmin=0, vmax=unified.max())
-    divider3 = make_axes_locatable(sp3)
-    cax3 = divider3.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(sp3, cax=cax3)
+    fig.colorbar(plt.imshow(mask[1], vmin=0, vmax=unified.max()))
 
     plt.savefig(os.path.join(outpath, image[image.rfind("/") + 1:]))
     plt.clf()
